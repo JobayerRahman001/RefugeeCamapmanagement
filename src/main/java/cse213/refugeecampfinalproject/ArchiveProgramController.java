0aeaea146d350c5fc50f5ee7f2365b2be29e0d7e
JobayerRahman001 <rjobayer9@gmail.com>
2025-07-31 01:41:27 +0600

    Added

---
+++ src/main/java/cse213/refugeecampfinalproject/ArchiveProgramController.java
@@ -0,0 +1,34 @@
+package cse213.refugeecampfinalproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ArchiveProgramController
+{
+    @javafx.fxml.FXML
+    private TableView archiveprogramTableView;
+    @javafx.fxml.FXML
+    private TableColumn programtypeColum;
+    @javafx.fxml.FXML
+    private TableColumn programnameColum;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void gobacktoeducationpanelOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void completeprogramOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void archiveprogramOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/cse213/refugeecampfinalproject/EnrollmentController.java
@@ -0,0 +1,27 @@
+package cse213.refugeecampfinalproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class EnrollmentController
+{
+    @javafx.fxml.FXML
+    private TableColumn programnameColum;
+    @javafx.fxml.FXML
+    private TableView enrollmentTableView;
+    @javafx.fxml.FXML
+    private TableColumn enrolledstudentColum;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void gobacktoeducationpanelOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadOnAction(ActionEvent actionEvent) {
+    }
+}
