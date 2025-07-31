54a2129d1e4043bd66826e11e61ffd696b6d0d73
Jarin-Tasmin <jarintasminshitol@gmail.com>
2025-07-31 15:35:01 +0600

    Added

---
+++ src/main/java/cse213/refugeecampfinalproject/QuarterlyImpactAnalysisController.java
@@ -0,0 +1,42 @@
+package cse213.refugeecampfinalproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class QuarterlyImpactAnalysisController
+{
+    @javafx.fxml.FXML
+    private TextField healthparcentTextField;
+    @javafx.fxml.FXML
+    private TextField enrollmentcountTextField;
+    @javafx.fxml.FXML
+    private Label impactheadingLabel;
+    @javafx.fxml.FXML
+    private TextField patientcountTextField;
+    @javafx.fxml.FXML
+    private TextArea remarksTextArea;
+    @javafx.fxml.FXML
+    private TextField infrastructuredateTextField;
+    @javafx.fxml.FXML
+    private TextField educationparcentTextField;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitreportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void newquarterreportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void gobacktoundashboardOnAction(ActionEvent actionEvent) {
+    }
+}
