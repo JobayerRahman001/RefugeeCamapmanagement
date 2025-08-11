331810f8364f070160063da075816083b3d687e6
JobayerRahman001 <rjobayer9@gmail.com>
2025-08-11 16:49:10 +0600

    Added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,25 +7,25 @@ module cse213.refugeecampfinalproject {
     exports cse213.refugeecampfinalproject;
 
     exports cse213.refugeecampfinalproject.DoctorAndEducationCoordinator;
-    opens cse213.refugeecampfinalproject.DoctorAndEducationCoordinator to javafx.fxml;
+    opens cse213.refugeecampfinalproject.DoctorAndEducationCoordinator to javafx.fxml,javafx.base;
 
     exports cse213.refugeecampfinalproject.Refugee;
-    opens cse213.refugeecampfinalproject.Refugee to javafx.fxml;
+    opens cse213.refugeecampfinalproject.Refugee to javafx.fxml,javafx.base;
 
     exports cse213.refugeecampfinalproject.Admin;
-    opens cse213.refugeecampfinalproject.Admin to javafx.fxml;
+    opens cse213.refugeecampfinalproject.Admin to javafx.fxml,javafx.base;
 
 
     exports cse213.refugeecampfinalproject.Volunteer;
-    opens cse213.refugeecampfinalproject.Volunteer to javafx.fxml;
+    opens cse213.refugeecampfinalproject.Volunteer to javafx.fxml,javafx.base;
 
 
     exports cse213.refugeecampfinalproject.ResourceManager;
-    opens cse213.refugeecampfinalproject.ResourceManager to javafx.fxml;
+    opens cse213.refugeecampfinalproject.ResourceManager to javafx.fxml,javafx.base;
 
     exports cse213.refugeecampfinalproject.AccountantAndLogistics;
-    opens cse213.refugeecampfinalproject.AccountantAndLogistics to javafx.fxml;
+    opens cse213.refugeecampfinalproject.AccountantAndLogistics to javafx.fxml,javafx.base;
 
     exports cse213.refugeecampfinalproject.UNHCR;
-    opens cse213.refugeecampfinalproject.UNHCR to javafx.fxml;
+    opens cse213.refugeecampfinalproject.UNHCR to javafx.fxml,javafx.base;
 }
